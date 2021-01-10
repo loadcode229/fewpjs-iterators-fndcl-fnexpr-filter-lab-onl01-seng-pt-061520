6d9081b3bfa65133f79e15546f4c22dd5f8cfd0e
Your Name <you@example.com>
2021-01-10 18:01:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const findMatching = (drivers, string) => {
+    return drivers.filter( find_name => {
+        return (find_name.toLowerCase() === string.toLowerCase())
+    })
+}
+
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter(find_name => {
+        if (string.charAt(0) === find_name.charAt(0)){
+            return find_name
+        }
+    })
+}
+
+
+const matchName = (drivers, string) => {
+    return drivers.filter(find_name => {
+        if (find_name.name === string){
+            return find_name
+        }
+    })
+}
